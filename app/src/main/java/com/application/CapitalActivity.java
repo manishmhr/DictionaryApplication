3534a61e0c76d9367161418f4095bb25660b1a69
Softwarica College Lab <softwarica@softwarica.com>
2019-04-16 12:20:26 +0545

    fin com

--- app/src/main/java/com/application/CapitalActivity.java
+++ app/src/main/java/com/application/CapitalActivity.java
@@ -24,6 +24,7 @@ public class CapitalActivity extends AppCompatActivity {
         }
         else
         {
+
             Toast.makeText(this, "No Message", Toast.LENGTH_SHORT).show();
         }
     }
