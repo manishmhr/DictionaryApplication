6e2dd2ec70d56ffffed057a3f324866973ae0780
Softwarica College Lab <softwarica@softwarica.com>
2019-04-18 14:32:14 +0545

    week 5 completed + added images

--- app/src/main/java/com/application/RecyclerActivity.java
+++ app/src/main/java/com/application/RecyclerActivity.java
@@ -24,16 +24,19 @@ public class RecyclerActivity extends AppCompatActivity {
 
         //Create a list of Contact
         List<Contacts> contactsList = new ArrayList<>();
+        contactsList.add(new Contacts("Mr fox","9855555555",R.drawable.fox));
+        contactsList.add(new Contacts("Manish","9866666666",R.drawable.man));
+        contactsList.add(new Contacts("Mr Bear","9888888888",R.drawable.mrbear));
+        contactsList.add(new Contacts("Skull","no phone",R.drawable.mrskull));
+        contactsList.add(new Contacts("Penguin","no phone",R.drawable.penguin));
+        contactsList.add(new Contacts("Pikachu","no phone",R.drawable.pikachu));
+        contactsList.add(new Contacts("Rabbit","no phone",R.drawable.rabbit));
+        contactsList.add(new Contacts("Storm Trooper","no phone",R.drawable.trooper));
+        contactsList.add(new Contacts("Unicorn","no phone",R.drawable.unicorn));
         contactsList.add(new Contacts("saugat Malla","9855555555",R.drawable.saugat));
         contactsList.add(new Contacts("Bhuwan KC","9866666666",R.drawable.bhuwan));
         contactsList.add(new Contacts("Rajesh Dai","9888888888",R.drawable.rajesh));
         contactsList.add(new Contacts("Dayahang Rai","no phone",R.drawable.dahayang));
-        contactsList.add(new Contacts("saugat Malla","9855555555",R.drawable.saugat));
-        contactsList.add(new Contacts("Bhuwan KC","9866666666",R.drawable.bhuwan));
-        contactsList.add(new Contacts("Rajesh Dai","9888888888",R.drawable.rajesh));
-        contactsList.add(new Contacts("Dayahang Rai","no phone",R.drawable.dahayang));
-        contactsList.add(new Contacts("saugat Malla","9855555555",R.drawable.saugat));
-        contactsList.add(new Contacts("Bhuwan KC","9866666666",R.drawable.bhuwan));
 
         //pass this list to adapter
 
